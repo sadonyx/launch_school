ebc066a15fb06693c69f016d7fe5cf6726832e46
Adnan Shihabi <adnan@ashihabi.me>
2022-11-23 00:22:26 -0800

    Lesson 6 Folder Initialized. Tic Tac Toe Pt. 1 & 2 Completed.

---
+++ RB101/lesson6/tictactoe.rb
@@ -0,0 +1,56 @@
+INITIAL_MARKER = ' '
+PLAYER_MARKER = 'X'
+COMPUTER_MARKER = 'O'
+
+def prompt(msg)
+  puts "=> #{msg}"
+end
+
+def display_board(brd)
+  puts ""
+  puts "     |     |"
+  puts "  #{brd[1]}  |  #{brd[2]}  |  #{brd[3]}"
+  puts "     |     |"
+  puts "-----+-----+-----"
+  puts "     |     |"
+  puts "  #{brd[4]}  |  #{brd[5]}  |  #{brd[6]}"
+  puts "     |     |"
+  puts "-----+-----+-----"
+  puts "     |     |"
+  puts "  #{brd[7]}  |  #{brd[8]}  |  #{brd[9]}"
+  puts "     |     |"
+  puts ""
+end
+
+def initialize_board
+  new_board = {}
+  (1..9).each {|num| new_board[num] = INITIAL_MARKER}
+  new_board
+end
+
+def empty_squares(brd)
+  brd.keys.select{|num| brd[num] == INITIAL_MARKER}
+end
+
+def player_move!(brd)
+  square = ''
+  loop do
+    prompt "Choose a square (#{empty_squares(brd).join(', ')}):"
+    square = gets.chomp.to_i
+    break if empty_squares(brd).include?(square)
+    prompt "Sorry, that's not a valid choice."
+  end
+  
+  brd[square] = PLAYER_MARKER
+end
+
+def computer_move(brd)
+
+end
+
+board = initialize_board
+display_board(board)
+
+player_move!(board)
+computer_move(board)
+display_board(board)
