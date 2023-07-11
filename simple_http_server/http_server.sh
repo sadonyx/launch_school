c152ef4763ae45a76b37ff681b4bffdde672d0a7
Adnan Shihabi <adnan@ashihabi.me>
2023-07-11 00:25:11 -0700

    LaunchSchool Practice Problems up to LS170

--- simple_http_server/http_server.sh
+++ simple_http_server/http_server.sh
@@ -3,11 +3,34 @@
 function server () {
   while true
   do
-    read method path version
-    if 
+    message_arr=()
+    check=true
+    while $check
+    do
+      read line
+      message_arr+=($line)
+      if [[ "${#line}" -eq 1 ]]
+      then
+        check=false
+      fi
+    done
+    method=${message_arr[0]}
+    path=${message_arr[1]}
+    content_length=`wc -c './www/'$path`
+    if [[ $method = 'GET' ]]
+    then
+      if [[ -f "./www/$path" ]]
+      then
+        echo -ne "HTTP/1.1 200 OK\r\nContent-Type: text/html; charset=utf-8\r\nContent-Length: $content_length\r\n\r\n"; cat "./www/$path"
+      else
+        echo -ne 'HTTP/1.1 404 Not Found\r\nContent-Length: 0\r\n\r\n'
+      fi
+    else
+      echo -ne 'HTTP/1.1 400 Bad Request\r\nContent-Length: 0\r\n\r\n'
+    fi
   done
 }
 
 coproc SERVER_PROCESS { server; }
 
-netcat -lv 2345 <&${SERVER_PROCESS[0] >&${SERVER_PROCESS[1]}}
+netcat -lvk 2345 <&${SERVER_PROCESS[0]} >&${SERVER_PROCESS[1]}
